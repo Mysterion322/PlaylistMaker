6c6f345690ba35ee9c8ce5d7d05de6a1b02ecfc4
Vladislav Doronin <mysterion1337@yandex.ru>
2024-04-26 09:56:50 +0300

    10 sprint 1.01

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,4 +1,8 @@
 package com.example.playlistmaker
 
-class Track {
-}
+data class Track (
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String,
+    val artworkUrl100: String
+)
