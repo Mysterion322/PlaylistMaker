ed26a423d31786a1d592566b0790d84494c0fd24
Vladislav Doronin <mysterion1337@yandex.ru>
2024-05-04 17:04:18 +0300

    10 sprint 2.0

--- app/src/main/java/com/example/playlistmaker/TrackHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackHolder.kt
@@ -9,17 +9,17 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
-    val ivTrack: ImageView = itemView.findViewById(R.id.iv_track)
-    val tvTrackName: TextView = itemView.findViewById(R.id.tv_track_name)
-    val tvTrackArtist: TextView = itemView.findViewById(R.id.tv_track_artist)
-  //  val ivTermsOfUse: ImageView = itemView.findViewById(R.id.iv_terms_of_use)
+    private val ivTrack: ImageView = itemView.findViewById(R.id.iv_track)
+    private val tvTrackName: TextView = itemView.findViewById(R.id.tv_track_name)
+    private val tvTrackArtist: TextView = itemView.findViewById(R.id.tv_track_artist)
+  // private val ivTermsOfUse: ImageView = itemView.findViewById(R.id.iv_terms_of_use)
 
     fun bind(item: Track) {
         tvTrackName.text = item.trackName
         tvTrackArtist.text = item.artistName+" • "+item.trackTime
         Glide.with(itemView)
             .load(item.artworkUrl100)
-            .placeholder(R.drawable.musicicon2)
+            .placeholder(R.drawable.placeholder)
             .centerCrop()
             .transform(RoundedCorners(10))
             .into(ivTrack)
