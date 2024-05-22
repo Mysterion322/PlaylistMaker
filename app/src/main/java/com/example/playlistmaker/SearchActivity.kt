004b20204bad63cba8d5196b140cd992e5f473b7
Vladislav Doronin <mysterion1337@yandex.ru>
2024-05-22 07:55:21 +0300

    11 sprint 1.0

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -8,15 +8,38 @@ import android.text.TextWatcher
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnTouchListener
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var editTextSearch: EditText
+    private lateinit var notFound: LinearLayout
+    private lateinit var noInternet: LinearLayout
+    private var text: String = EMPTY
+    private val trackList = mutableListOf<Track>()
+    private val trackAdapter = TrackAdapter(trackList)
+
+    private val baseUrl = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val iTunesService = retrofit.create(TrackAPI::class.java)
 
 
     @SuppressLint("ClickableViewAccessibility")
@@ -24,7 +47,10 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val editTextSearch = findViewById<EditText>(R.id.search_edit_text)
+        editTextSearch = findViewById<EditText>(R.id.search_edit_text)
+        notFound = findViewById<LinearLayout>(R.id.ll_not_found)
+        noInternet = findViewById<LinearLayout>(R.id.ll_no_internet)
+        val updateButton = findViewById<Button>(R.id.update_button)
 
         editTextSearch.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS)
 
@@ -36,8 +62,11 @@ class SearchActivity : AppCompatActivity() {
                 if (event.rawX >= editTextSearch.getRight() - editTextSearch.getCompoundDrawables()
                         .get(drawableRight).getBounds().width()
                 ) {
-                    editTextSearch.setText("")
+                    editTextSearch.setText(EMPTY)
                     editTextSearch.setCompoundDrawablesWithIntrinsicBounds(R.drawable.search_small_image,0, R.drawable.search_image_layout, 0)
+                    trackList.clear()
+                    notFound.visibility = View.GONE
+                    noInternet.visibility = View.GONE
 
                     val view: View? = this.currentFocus
 
@@ -72,6 +101,7 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable?) {
+                text = s.toString()
                 // Здесь можно добавить любой нужный код после изменения текста
             }
         })
@@ -82,67 +112,71 @@ class SearchActivity : AppCompatActivity() {
             onBackPressedDispatcher.onBackPressed()
         }
 
-
-        val rvTrackList = findViewById<RecyclerView>(R.id.rv_track_list)
-        rvTrackList.apply {
-            adapter = trackAdapter
-            layoutManager =
-                LinearLayoutManager(this@SearchActivity, LinearLayoutManager.VERTICAL, false)
+        updateButton.setOnClickListener {
+            noInternet.visibility = View.GONE
+            apiRequest(text)
         }
 
-        trackAdapter.items = trackList
-
+        editTextSearch.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                apiRequest(text)
+                true
+            }
+            false
+        }
 
     }
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
         val editTextSearch = findViewById<EditText>(R.id.search_edit_text)
-        outState.putString("SEARCH_TEXT", editTextSearch.text.toString())
+        outState.putString(KEY, editTextSearch.text.toString())
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
 
         val editTextSearch = findViewById<EditText>(R.id.search_edit_text)
-        editTextSearch.setText(savedInstanceState.getString("SEARCH_TEXT", ""))
+        editTextSearch.setText(savedInstanceState.getString(KEY, EMPTY))
+    }
+
+    fun apiRequest(text: String) {
+        iTunesService.getTrack(text).enqueue(object : Callback<ResponseTracks> {
+            override fun onResponse(call: Call<ResponseTracks>, response: Response<ResponseTracks>) {
+
+                val tracksFromResp = response.body()?.results
+
+                if (response.isSuccessful && tracksFromResp != null) {
+                    if (tracksFromResp.isEmpty()) {
+                        notFound.visibility = View.VISIBLE
+                        noInternet.visibility = View.GONE
+                        trackList.clear()
+                    } else {
+                        trackList.clear()
+                        trackList.addAll(tracksFromResp.toMutableList())
+                        notFound.visibility = View.GONE
+                        noInternet.visibility = View.GONE
+                        val rvTrackList = findViewById<RecyclerView>(R.id.rv_track_list)
+                        rvTrackList.adapter = trackAdapter
+                        rvTrackList.layoutManager =
+                            LinearLayoutManager(this@SearchActivity, LinearLayoutManager.VERTICAL, false)
+                    }
+                }
+
+            }
+
+            override fun onFailure(p0: Call<ResponseTracks>, p1: Throwable) {
+                trackList.clear()
+                noInternet.visibility = View.VISIBLE
+                notFound.visibility = View.GONE
+            }
+        })
     }
 
 
-
-    val trackList = arrayListOf<Track>(
-        Track(
-            "Smells Like Teen Spirit",
-            "Nirvana",
-            "5:01",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-        ),Track(
-            "Billie Jean",
-            "Michael Jackson",
-            "4:35",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-        ),Track(
-            "Stayin' Alive",
-            "Bee Gees",
-            "4:10",
-            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-        ),Track(
-            "Whole Lotta Love",
-            "Led Zeppelin",
-            "5:33",
-            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-        ),Track(
-            "Sweet Child O'Mine",
-            "Guns N' Roses",
-            "5:03",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-        )
-    )
-
-    val trackAdapter = TrackAdapter()
-
-
-
-
+    companion object {
+        private const val KEY = "SEARCH_TEXT"
+        private const val EMPTY = ""
+    }
 
 }
